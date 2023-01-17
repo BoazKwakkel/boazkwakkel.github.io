23e17953de800636aae62c3bd85fa36dc078e3e8
Yvysanna <yvette.violetta@web.de>
2023-01-17 13:43:47 +0100

    added comments

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// Generates random choice of images to view from our image folder
+// TODO: Making tiny images clickable so that they become main image
+// TODO: Generating clickable rectangle areas on main image
+// TODO: Linking clickable rectangle areas on main image to image choice -> trigger random_imglink function
+
+
+/**
+ * Generates random choice of images to view from our image folder
+ * Makes use of local folder /paintings at the moment!
+ */
 function random_imglink() {
 
     for (let i = 1; i < 7; i++) {
@@ -9,20 +17,24 @@ function random_imglink() {
     }
 }
 
-var checker = function checkView(el) {
-    if (el.style.display == 'none') {
-        el.style.display = 'block'
-    } else {
-        el.style.display = 'none'
-    }
-}
-
-
-// Call function
+// Call function to generate images
 random_imglink()
 
-// Toggle view for images function
+
+/**
+ * Toggles the visibility of the 6 small images depending on if the main image is clicked
+ * Requires that images cointain style attribute {display: block} or {display: none}
+ */
 function toggle() {
-    // Find all img elements that can be toggled and toggle them
-    document.querySelectorAll(".choices").forEach(checker)
+
+    // Find all img elements that can be toggled
+    document.querySelectorAll(".choices").forEach(function checkView(el) {
+        
+        // Toggle visibility of images depending on their state
+        if (el.style.display == 'none') {
+            el.style.display = 'block'
+          } else {
+            el.style.display = 'none'
+          }
+    })
 }
